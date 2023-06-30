1b9cd44aa7f957de017d8cb436f2075c4f4275ed
Callum Bryant <callum.l.bryant@gmail.com>
2023-06-30 22:41:25 +1000

    working build

---
+++ src/override/App.vue
@@ -0,0 +1,13 @@
+<template>
+  <hello-world />
+  <h1>This is a test new tab page</h1>
+</template>
+
+<script>
+import HelloWorld from '@/components/HelloWorld.vue'
+
+export default {
+  name: 'App',
+  components: { HelloWorld }
+}
+</script>
--- vue.config.js
+++ vue.config.js
@@ -1,53 +1,27 @@
-/**
- * Created by kelvinsun on 18/9/29.
- *
- * vue-cli config file.
- */
-
-'use strict';
-
-const CopyWebpackPlugin = require('copy-webpack-plugin');
-
-module.exports = {
-    // 因为这个项目是打包发布的 app, 不是线上资源, 所以不需要哈希
-    filenameHashing: false,
-    pages: {
-        popup: {
-            entry: 'src/popup.js',
-            template: 'public/popup.html',
-        },
-        background: {
-            entry: 'src/background.js',
-            template: 'public/background.html',
-        },
+const { defineConfig } = require('@vue/cli-service')
+module.exports = defineConfig({
+  transpileDependencies: true,
+  pages: {
+    options: {
+      template: 'public/browser-extension.html',
+      entry: './src/options/main.js',
+      title: 'Options'
     },
-    // 开发的时候为 true, 发布时为 false
-    productionSourceMap: 'development' === process.env.NODE_ENV,
-    configureWebpack: {
-        plugins: [
-            new CopyWebpackPlugin([
-                { from: 'public/manifest.json', to: 'manifest.json', },
-                { from: 'src/image/*.png', to: 'image/[name].png', },
-            ]),
-        ],
+    override: {
+      template: 'public/browser-extension.html',
+      entry: './src/override/main.js',
+      title: 'Override'
     },
-    // chainWebpack 修改编译目标目录
-    // https://segmentfault.com/q/1010000016475212
-    chainWebpack: config => {
-        config.module
-            .rule('images')
-            .use('url-loader')
-            .tap(() => {
-                // 日, 这里不应该放数组
-                return {
-                    limit: 4096,
-                    fallback: {
-                        loader: 'file-loader',
-                        options: {
-                            name: 'image/[name].[ext]',
-                        },
-                    },
-                };
-            });
-    },
-};
+    standalone: {
+      template: 'public/browser-extension.html',
+      entry: './src/standalone/main.js',
+      title: 'Standalone',
+      filename: 'index.html'
+    }
+  },
+  pluginOptions: {
+    browserExtension: {
+      componentOptions: {}
+    }
+  }
+})
